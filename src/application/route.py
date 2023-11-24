e219750dc5d68662670210d9562d16cefadcb421
Polzovat123 <sa8.sab@yandex.ru>
2023-11-24 18:13:31 +0500

    append route

--- src/application/route.py
+++ src/application/route.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from additional_routes.recomendadtion_course.route import router as course_router
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
